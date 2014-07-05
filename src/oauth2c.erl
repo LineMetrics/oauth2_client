1110de336d9d8a749ea50d4651f0a9550899f44a
Peter Olson <peter@overtype.com>
2014-07-05 00:56:44 -0400

    updating the oauth client

--- src/oauth2c.erl
+++ src/oauth2c.erl
@@ -27,13 +27,16 @@
 -module(oauth2c).
 
 -export([
-         retrieve_access_token/4, retrieve_access_token/5
-         ,request/3
-         ,request/4
-         ,request/5
-         ,request/6
-         ,request/7
-        ]).
+    retrieve_access_token/4, 
+    retrieve_access_token/5, 
+    retrieve_access_token/6,
+    retrieve_access_token/7
+    ,request/3
+    ,request/4
+    ,request/5
+    ,request/6
+    ,request/7
+    ]).
 
 -type method()       :: head | get | put | post | trace | options | delete.
 -type url()          :: binary().
@@ -48,8 +51,8 @@
 -type proplist()     :: [property()].
 -type body()         :: proplist().
 -type response()     :: {ok, Status::status_code(), Headers::headers(), Body::body()} |
-                        {error, Status::status_code(), Headers::headers(), Body::body()} |
-                        {error, Reason::reason()}.
+    {error, Status::status_code(), Headers::headers(), Body::body()} |
+    {error, Reason::reason()}.
 
 
 -define(DEFAULT_ENCODING, json).
@@ -61,38 +64,81 @@
         refresh_token = undefined :: binary(),
         id            = undefined :: binary(),
         secret        = undefined :: binary(),
-        scope         = undefined :: binary()
-}).
+        scope         = undefined :: binary(),
+        redirect_uri  = undefined :: binary(),
+        expires_in    = undefined :: integer(),
+        token_type    = undefined :: binary(),
+        auth_code     = undefined :: binary()
+        }).
 
 
 %%% API ========================================================================
 
 
 -spec retrieve_access_token(Type, URL, ID, Secret) ->
-    {ok, Headers::headers(), #client{}} | {error, Reason :: binary()} when
+    {ok, Headers::headers(), #client{}} | 
+    {ok, Headers::headers(), #client{}, RefreshToken::binary()} | 
+    {error, Reason :: binary()} when
     Type   :: at_type(),
     URL    :: url(),
     ID     :: binary(),
     Secret :: binary().
 retrieve_access_token(Type, Url, ID, Secret) ->
-    retrieve_access_token(Type, Url, ID, Secret, undefined).
+    retrieve_access_token(Type, Url, ID, Secret, undefined,undefined,undefined).
 
+%% left in for backwards compatibility
 -spec retrieve_access_token(Type, URL, ID, Secret, Scope) ->
-    {ok, Headers::headers(), #client{}} | {error, Reason :: binary()} when
+    {ok, Headers::headers(), #client{}} | 
+    {ok, Headers::headers(), #client{}, RefreshToken::binary()} | 
+    {error, Reason :: binary()} when
     Type   :: at_type(),
     URL    :: url(),
     ID     :: binary(),
     Secret :: binary(),
     Scope  :: binary() | undefined.
 retrieve_access_token(Type, Url, ID, Secret, Scope) ->
+    retrieve_access_token(Type, Url, ID, Secret, Scope,undefined,undefined).
+
+-spec retrieve_access_token(Type, URL, ID, Secret, Scope, RedirectURI) ->
+    {ok, Headers::headers(), #client{}} | 
+    {ok, Headers::headers(), #client{}, RefreshToken::binary()} | 
+    {error, Reason :: binary()} when
+    Type        :: at_type(),
+    URL         :: url(),
+    ID          :: binary(),
+    Secret      :: binary(),
+    Scope       :: binary() | undefined,
+    RedirectURI :: binary() | undefined.
+retrieve_access_token(Type, Url, ID, Secret, Scope, RedirectURI) ->
+    retrieve_access_token(Type, Url, ID, Secret, Scope,RedirectURI,undefined).
+
+
+-spec retrieve_access_token(Type, URL, ID, Secret, Scope, RedirectURI, AuthCode) ->
+    {ok, Headers::headers(), #client{}} | 
+    {ok, Headers::headers(), #client{}, RefreshToken::binary()} | 
+    {error, Reason :: binary()} when
+    Type        :: at_type(),
+    URL         :: url(),
+    ID          :: binary(),
+    Secret      :: binary(),
+    Scope       :: binary() | undefined,
+    RedirectURI :: binary() | undefined,
+    AuthCode    :: binary() | undefined.
+retrieve_access_token(Type, Url, ID, Secret, Scope, RedirectURI, AuthCode) ->
     Client = #client{
-                     grant_type = Type
-                     ,auth_url  = Url
-                     ,id        = ID
-                     ,secret    = Secret
-                     ,scope     = Scope
-                    },
+            grant_type     = Type
+            ,auth_url      = Url
+            ,id            = ID
+            ,secret        = Secret
+            ,scope         = Scope
+            ,redirect_uri  = RedirectURI
+            ,auth_code     = AuthCode
+            },
     do_retrieve_access_token(Client).
+
+
+
+
 
 -spec request(Method, Url, Client) -> Response::response() when
     Method :: method(),
@@ -150,14 +196,14 @@ request(Method, Type, Url, Expect, Headers, Body, Client) ->
 
 do_retrieve_access_token(#client{grant_type = <<"password">>} = Client) ->
     Payload0 = [
-                {<<"grant_type">>, Client#client.grant_type}
-                ,{<<"username">>, Client#client.id}
-                ,{<<"password">>, Client#client.secret}
-               ],
+            {<<"grant_type">>, Client#client.grant_type}
+            ,{<<"username">>, Client#client.id}
+            ,{<<"password">>, Client#client.secret}
+            ],
     Payload = case Client#client.scope of
-                 undefined -> Payload0;
-                 Scope -> [{<<"scope">>, Scope}|Payload0]
-              end,
+        undefined -> Payload0;
+        Scope -> [{<<"scope">>, Scope}|Payload0]
+    end,
     case restc:request(post, percent, binary_to_list(Client#client.auth_url), [200], [], Payload) of
         {ok, _, Headers, Body} ->
             AccessToken = proplists:get_value(<<"access_token">>, Body),
@@ -165,21 +211,21 @@ do_retrieve_access_token(#client{grant_type = <<"password">>} = Client) ->
             Result = case RefreshToken of
                 undefined ->
                     #client{
-                            grant_type    = Client#client.grant_type
-                            ,auth_url     = Client#client.auth_url
-                            ,access_token = AccessToken
-                            ,id           = Client#client.id
-                            ,secret       = Client#client.secret
-                            ,scope        = Client#client.scope
-                            };
+                        grant_type    = Client#client.grant_type
+                        ,auth_url     = Client#client.auth_url
+                        ,access_token = AccessToken
+                        ,id           = Client#client.id
+                        ,secret       = Client#client.secret
+                        ,scope        = Client#client.scope
+                        };
                 _ ->
                     #client{
-                            grant_type     = Client#client.grant_type
-                            ,auth_url      = Client#client.auth_url
-                            ,access_token  = AccessToken
-                            ,refresh_token = RefreshToken
-                            ,scope         = Client#client.scope
-                            }
+                        grant_type     = Client#client.grant_type
+                        ,auth_url      = Client#client.auth_url
+                        ,access_token  = AccessToken
+                        ,refresh_token = RefreshToken
+                        ,scope         = Client#client.scope
+                        }
             end,
             {ok, Headers, Result};
         {error, _, _, Reason} ->
@@ -187,29 +233,116 @@ do_retrieve_access_token(#client{grant_type = <<"password">>} = Client) ->
         {error, Reason} ->
             {error, Reason}
     end;
+
 do_retrieve_access_token(#client{grant_type = <<"client_credentials">>,
-                                 id = Id, secret = Secret} = Client) ->
+                id = Id, secret = Secret} = Client) ->
     Payload0 = [{<<"grant_type">>, Client#client.grant_type}],
     Payload = case Client#client.scope of
-                  undefined ->
-                      Payload0;
-                  Scope ->
-                      [{<<"scope">>, Scope}|Payload0]
-              end,
+        undefined ->
+            Payload0;
+        Scope ->
+            [{<<"scope">>, Scope}|Payload0]
+    end,
     Auth = base64:encode(<<Id/binary, ":", Secret/binary>>),
     Header = [{"Authorization", binary_to_list(<<"Basic ", Auth/binary>>)}],
     case restc:request(post, percent, binary_to_list(Client#client.auth_url),
-                       [200], Header, Payload) of
+            [200], Header, Payload) of
         {ok, _, Headers, Body} ->
             AccessToken = proplists:get_value(<<"access_token">>, Body),
+            RefreshToken = proplists:get_value(<<"refresh_token">>, Body),
             Result = #client{
-                             grant_type    = Client#client.grant_type
-                             ,auth_url     = Client#client.auth_url
-                             ,access_token = AccessToken
-                             ,id           = Client#client.id
-                             ,secret       = Client#client.secret
-                             ,scope        = Client#client.scope
-                            },
+                    grant_type      = Client#client.grant_type
+                    ,auth_url       = Client#client.auth_url
+                    ,access_token   = AccessToken
+                    ,refresh_token  = RefreshToken
+                    ,id             = Client#client.id
+                    ,secret         = Client#client.secret
+                    ,scope          = Client#client.scope
+                    },
+            {ok, Headers, Result};
+        {error, _, _, Reason} ->
+            {error, Reason};
+        {error, Reason} ->
+            {error, Reason}
+    end;
+
+do_retrieve_access_token(#client{
+                grant_type = <<"authorization_code">>,
+                id = Id, 
+                secret = Secret,
+                redirect_uri = RedirectURI,
+                auth_code = AuthorizationCode
+                } = Client) ->
+    Payload = [
+            {<<"grant_type">>, Client#client.grant_type},
+            {<<"code">>,AuthorizationCode},
+            {<<"client_id">>,Id},
+            {<<"client_secret">>,Secret},
+            {<<"redirect_uri">>,RedirectURI}
+            ],
+    
+    case restc:request(post, percent, binary_to_list(Client#client.auth_url),
+            [200], [], Payload) of
+        {ok, _, Headers, Body} ->
+            io:format("body is: ~p~n", [Body]),
+            io:format("decoded body: ~p~n", [jsx:decode(Body) ]),
+            B = jsx:decode(Body),
+            AccessToken = proplists:get_value(<<"access_token">>, B),
+            RefreshToken = proplists:get_value(<<"refresh_token">>, B),
+            TokenType = proplists:get_value(<<"token_type">>, B),
+            ExpiresIn = proplists:get_value(<<"expires_in">>, B),
+            
+            
+            Result = #client{
+                    grant_type      = Client#client.grant_type,
+                    auth_url        = Client#client.auth_url,
+                    access_token    = AccessToken,
+                    refresh_token   = RefreshToken,
+                    token_type      = TokenType,
+                    expires_in      = calculate_expiry(ExpiresIn),
+                    id              = Client#client.id,
+                    secret          = Client#client.secret,
+                    scope           = Client#client.scope
+                    },
+            {ok, Headers, Result, RefreshToken};
+        {error, _, _, Reason} ->
+            {error, Reason};
+        {error, Reason} ->
+            {error, Reason}
+    end;
+
+do_retrieve_access_token(#client{
+                grant_type = <<"refresh_token">>,
+                id = Id, 
+                secret = Secret,
+                refresh_token = RefreshToken
+                } = Client) ->
+    Payload = [
+            {<<"grant_type">>, Client#client.grant_type},
+            {<<"refresh_token">>, RefreshToken},
+            {<<"client_id">>, Id},
+            {<<"client_secret">>, Secret}
+            ],
+    
+    case restc:request(post, percent, binary_to_list(Client#client.auth_url),
+            [200], [], Payload) of
+        {ok, _, Headers, Body} ->
+            B = jsx:decode(Body),
+            AccessToken = proplists:get_value(<<"access_token">>, B),
+            TokenType = proplists:get_value(<<"token_type">>, B),
+            ExpiresIn = proplists:get_value(<<"expires_in">>, B),
+            
+            Result = #client{
+                    grant_type      = Client#client.grant_type,
+                    auth_url        = Client#client.auth_url,
+                    access_token    = AccessToken,
+                    token_type      = TokenType,
+                    refresh_token   = RefreshToken,
+                    expires_in      = calculate_expiry(ExpiresIn),
+                    id              = Client#client.id,
+                    secret          = Client#client.secret,
+                    scope           = Client#client.scope
+                    },
             {ok, Headers, Result};
         {error, _, _, Reason} ->
             {error, Reason};
@@ -218,9 +351,38 @@ do_retrieve_access_token(#client{grant_type = <<"client_credentials">>,
     end.
 
 do_request(Method, Type, Url, Expect, Headers, Body, Client) ->
-    Headers2 = add_auth_header(Headers, Client),
-    {restc:request(Method, Type, binary_to_list(Url), Expect, Headers2, Body), Client}.
+    Client2 = check_expired(Client),
+    Headers2 = add_auth_header(Headers, Client2),
+    {restc:request(Method, Type, binary_to_list(Url), Expect, Headers2, Body), Client2}.
 
-add_auth_header(Headers, #client{access_token = AccessToken}) ->
-    AH = {"Authorization", binary_to_list(<<"token ", AccessToken/binary>>)},
+add_auth_header(Headers, #client{access_token = AccessToken,token_type = TokenType}) ->
+    case TokenType of 
+        undefined -> Ttype = <<"token ">>;
+        _ -> Ttype = <<TokenType/binary, " ">> 
+    end, 
+    AH = {"Authorization", binary_to_list(<<Ttype/binary, AccessToken/binary>>)},
     [AH | proplists:delete("Authorization", Headers)].
+
+calculate_expiry(Seconds) ->
+    Now = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
+    Now + Seconds.
+
+check_expired(#client{expires_in = ExpiresIn} = Client) ->
+    Now = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
+    
+    Test = ((ExpiresIn - Now) > 0),
+    case Test of
+        true -> Client;
+        false -> TmpClient = #client{
+                    grant_type    = <<"refresh_token">>,
+                    auth_url      = Client#client.auth_url,
+                    refresh_token = Client#client.refresh_token,
+                    id            = Client#client.id,
+                    secret        = Client#client.secret,
+                    scope         = Client#client.scope
+                    },
+            io:format("requesting new token~n"),
+            {ok, _, C} = do_retrieve_access_token(TmpClient),
+            io:format("new token: ~p~n", [C]),
+            C   
+    end.
